f1323d065f77afd409911e670cae408e0925bf45
HopeWiddows <CCC012108@coderacademy.edu.au>
2021-07-19 13:33:16 +1000

    Initializing snake body in game

--- src/snake.py
+++ src/snake.py
@@ -4,13 +4,29 @@ from curses import textpad #simple textbox editing widget
 #Initialisation
 #screen = curses.initscr() # initializes screen
 
- #screen setup
+#main game 
 def main(stdscr):
+     #screen setup
     curses.curs_set(0) #disabling the blinking cursor
     screen_height, screen_width = stdscr.getmaxyx() #allowing screen height and width
     box = [[3,3], [screen_height-3, screen_width-3]] #setting up a box with coordinates for game screen
     textpad.rectangle(stdscr, box[0][0], box[0][1], box[1][0], box[1][1]) #setting up the rectangle for game play on screen
-    stdscr.refresh() #will update screen with any new changes
+    
+    #stdscr.refresh() #will update screen with any new changes
+    #stdscr.getch() #waits for user input before exiting
+
+    #initial state of snake
+    snake_body = [[screen_height//2, screen_width//2+1], [screen_height//2, screen_width//2], [screen_height//2, screen_width//2-1]] #snake starts with three parts to it's body and uses x axis to startin center of game screen
+    direction = curses.KEY_RIGHT #snake will start moving right at game start
+
+    for y, x in snake_body:
+        stdscr.addstr(y, x, '#')
+
     stdscr.getch() #waits for user input before exiting
 
+
+
+
 curses.wrapper(main)
+
+
