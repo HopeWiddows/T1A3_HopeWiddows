59f2e6dd4c9236c59fb568ffa5829e75dc881db8
HopeWiddows <CCC012108@coderacademy.edu.au>
2021-07-20 22:05:03 +1000

    Updating script file

--- src/bash_script.sh
+++ src/bash_script.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-if ! [[ -x "$(command -v python)" ]]
+if ! [[ -x "$(command -v python3)" ]]
 then
   echo 'Error: 
-    This program runs on Python, but it looks like Python is not installed.
-    To install Python, check out https://installpython3.com/' >&2
+    This program runs on Python3, but it looks like Python is not installed on your system.
+    To install Python3, check out https://installpython3.com/' >&2
   exit 1
 fi
 
-if [[ $1 == "snake" ]]
-then
-    python3 snake.py
-fi
+
+sudo python3 snake.py $1
